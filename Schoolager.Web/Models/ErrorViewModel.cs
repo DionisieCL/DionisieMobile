c77927d0789aef40999f311bd0dfed743334d552
duartrosar <74115478+duartrosar@users.noreply.github.com>
2022-11-04 14:34:06 +0000

    Add project files.

---
+++ Schoolager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Schoolager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
