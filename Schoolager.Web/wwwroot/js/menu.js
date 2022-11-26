d67e25330a720c487e6a6dcf6be412f7da8523bd
duartrosar <74115478+duartrosar@users.noreply.github.com>
2022-11-26 17:36:50 +0000

    Sidebar and navbar

---
+++ Schoolager.Web/wwwroot/js/menu.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    let isOpen = true;
+
+    $("#burgerBtn").click(function () {
+        if (isOpen) {
+            $("#mainContent").addClass("closed");
+            $("#sidebar").addClass("closed");
+            $("#navbar").addClass("closed");
+            isOpen = false
+        } else {
+            $("#mainContent").removeClass("closed");
+            $("#sidebar").removeClass("closed");
+            $("#navbar").removeClass("closed");
+            isOpen = true;
+        }
+
+        console.log("hhelo");
+    });
+});
