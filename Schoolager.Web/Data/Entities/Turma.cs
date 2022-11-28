d3650bf0ab643a2d58d9b8f5debe5a086b5af557
Alexandru Iordache <alexmadaliniordache@gmail.com>
2022-11-28 19:40:04 +0000

    Names in sidebad

--- Schoolager.Web/Data/Entities/Turma.cs
+++ Schoolager.Web/Data/Entities/Turma.cs
@@ -6,7 +6,7 @@ namespace Schoolager.Web.Data.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        //Appointment
         public ICollection<Student> Students { get; set; }
 
         public ICollection<SubjectTurma> SubjectTurma;
