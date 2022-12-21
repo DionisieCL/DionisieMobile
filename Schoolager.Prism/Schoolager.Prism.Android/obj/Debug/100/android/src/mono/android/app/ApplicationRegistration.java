bddd1b5167640986c0db82c91762b0281e4d1f5c
tatiana-clim <104533969+DionisieCL@users.noreply.github.com>
2022-12-21 13:04:55 +0000

    Update

---
+++ Schoolager.Prism/Schoolager.Prism.Android/obj/Debug/100/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Schoolager.Prism.Droid.MainApplication, Schoolager.Prism.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64170d37dbc030db82.MainApplication.class, crc64170d37dbc030db82.MainApplication.__md_methods);
+		
+	}
+}
