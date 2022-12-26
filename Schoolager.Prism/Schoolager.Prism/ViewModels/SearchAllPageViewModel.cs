ff8d58fc71aad8d3d66cd7b40b15918f1b7a8d14
tatiana-clim <104533969+DionisieCL@users.noreply.github.com>
2022-12-26 17:23:23 +0000

    SearchAllPage

--- Schoolager.Prism/Schoolager.Prism/ViewModels/SearchAllPageViewModel.cs
+++ Schoolager.Prism/Schoolager.Prism/ViewModels/SearchAllPageViewModel.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Xamarin.Forms;
 using Schoolager.Prism.Views;
+using Schoolager.Prism.Models;
+using Schoolager.Prism.Services;
+using Xamarin.Essentials;
 
 namespace Schoolager.Prism.ViewModels
 {
@@ -13,22 +16,59 @@ namespace Schoolager.Prism.ViewModels
     {
         private DelegateCommand _searchCommand;
 
-        private string _response;
-       
-        public FavoritesPageViewModel(INavigationService navigationService) : base(navigationService)
+
+        private string _searchItem;
+        private string _search;
+        private readonly IApiServices _apiService;
+        private bool _enabled;
+
+        private WeatherResponse _weatherDetail;
+
+        public FavoritesPageViewModel(INavigationService navigationService, IApiServices apiServices) : base(navigationService)
         {
             Title = "Search";
-
+            _apiService = apiServices;
+            _enabled = false;
         }
 
-        public string Response { 
-            get => _response;
-            set => SetProperty(ref (_response), value); }
+        public string SearchItem
+        { 
+            get => _searchItem;
+            set => SetProperty(ref (_searchItem), value); }
+
+        public WeatherResponse WeatherDetail
+        {
+            get => _weatherDetail;
+            set => SetProperty(ref (_weatherDetail), value);
+        }
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set => SetProperty(ref (_enabled), value);
+
+        }
+        public string Search {
+            get => _search;
+            set => SetProperty(ref (_search), value);
+        }
+    
         public DelegateCommand SearchCommand => _searchCommand ?? (_searchCommand = new DelegateCommand(ButtonClicked));
 
-        public void ButtonClicked()
+        public async void ButtonClicked()
         {
-            Response = "Teste";
+            string urlBase = App.Current.Resources["UrlAPIWeather"].ToString();
+            string key = App.Current.Resources["KEYWeather"].ToString();
+            string servicePrefix = "weather?q=" + Search;
+            string controller = "&appid=" + key + "&units=metric";
+            Response response = await _apiService.GetWeather(urlBase, servicePrefix, controller);
+            SearchItem = Search;
+            Search= null;
+            WeatherResponse weather = (WeatherResponse)response.Result;
+            string icon = weather.Weather[0].Icon;
+            weather.Weather[0].Icon= "http://openweathermap.org/img/wn/" + icon + "@4x.png";
+            WeatherDetail = weather;
+            Enabled = true;
         }
 
     }
