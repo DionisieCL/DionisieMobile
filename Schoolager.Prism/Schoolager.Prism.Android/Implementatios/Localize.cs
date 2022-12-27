f3953b5ac7afdbc5be45c90e2f2424d95549e78c
tatiana-clim <104533969+DionisieCL@users.noreply.github.com>
2022-12-27 18:31:54 +0000

    Languages Android

---
+++ Schoolager.Prism/Schoolager.Prism.Android/Implementatios/Localize.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Schoolager.Prism.Helpers;
+using Schoolager.Prism.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Schoolager.Prism.Droid.Implementatios.Localize))]
+namespace Schoolager.Prism.Droid.Implementatios
+{
+    public class Localize : ILocalize
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            string netLanguage = "en";
+            Java.Util.Locale andoirdLocale = Java.Util.Locale.Default;
+            netLanguage = AndroidToDotnetLanguage(andoirdLocale.ToString().Replace("_", "-"));
+
+            CultureInfo ci = null;
+            try
+            {
+                ci = new CultureInfo(netLanguage);
+            }
+            catch (CultureNotFoundException)
+            {
+                try
+                {
+                    string fallback = ToDotnetFallbackLanguage(new PlatformCulture(netLanguage));
+                    ci = new CultureInfo(fallback);
+                }
+                catch (CultureNotFoundException)
+                {
+                    ci = new CultureInfo("en");
+                }
+            }
+            return ci;
+        }
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+        private string AndroidToDotnetLanguage(string androidLanguage)
+        {
+            string netLanguage = androidLanguage;
+
+            switch (androidLanguage)
+            {
+                case "ms-BN":
+                case "ms-MY":
+                case "ms-SV":
+                    netLanguage = "ms";
+                    break;
+                case "in-ID":
+                    netLanguage = "id-ID";
+                    break;
+                case "gsw-CH":
+                    netLanguage = "de-CH";
+                    break;
+            }
+            return netLanguage;
+        }
+
+        private string ToDotnetFallbackLanguage(PlatformCulture platCulture)
+        {
+            string netLanguage = platCulture.LanguageCode;
+            switch (platCulture.LanguageCode)
+            {
+                case "gsw":
+                    netLanguage = "de-CH";
+                    break;
+            }
+            return netLanguage;
+        }
+    }
+}
